41365f20d4f981df46dd75f0c4ee115fa7c85b3d
vaaast_lake <kown9512@gmail.com>
2024-02-01 14:50:15 +0900

    sw_12510__special-sort with selectionSort

--- Algorithm_py/sw_12510__special-sort.py
+++ Algorithm_py/sw_12510__special-sort.py
@@ -2,38 +2,24 @@ import sys
 
 sys.stdin = open('./sw_12510__special-sort__input.txt', 'r')
 
-def merge_sort(arr):
-    if len(arr) < 2:
-        return arr
-
-    mid = len(arr) // 2
-    low_arr = merge_sort(arr[:mid])
-    high_arr = merge_sort(arr[mid:])
-
-    merged_arr = []
-    l = h = 0
-    while l < len(low_arr) and h < len(high_arr):
-        if low_arr[l] < high_arr[h]:
-            merged_arr.append(low_arr[l])
-            l += 1
-        else:
-            merged_arr.append(high_arr[h])
-            h += 1
-    merged_arr += low_arr[l:]
-    merged_arr += high_arr[h:]
-    return merged_arr
-
+def selectionSort(lst, lst_len):
+    for i in range(lst_len-1):
+        minIdx = i
+        for j in range(i+1, lst_len):
+            if lst[minIdx] > lst[j]:
+                minIdx = j
+        lst[i], lst[minIdx] = lst[minIdx], lst[i]
 
 for ts in range(int(input())):
     N = int(input())
     nums = list(map(int, input().split()))
     nums_len = len(nums)
-    sorted_nums = merge_sort(nums)
+    selectionSort(nums, nums_len)
 
     result = []
     for i in range(nums_len-1, -1, -1):
-        result.append(sorted_nums[i])
-        result.append(sorted_nums[nums_len - 1 - i])
+        result.append(nums[i])
+        result.append(nums[nums_len - 1 - i])
         if len(result) == 10:
             break
 
