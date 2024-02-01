4305a06ac594cb170f49e73b2876c59c8c5678d9
vaaast_lake <kown9512@gmail.com>
2024-02-01 14:58:07 +0900

    sw_12510__special-sort with bubbleSort

--- Algorithm_py/sw_12510__special-sort.py
+++ Algorithm_py/sw_12510__special-sort.py
@@ -2,19 +2,17 @@ import sys
 
 sys.stdin = open('./sw_12510__special-sort__input.txt', 'r')
 
-def selectionSort(lst, lst_len):
-    for i in range(lst_len-1):
-        minIdx = i
-        for j in range(i+1, lst_len):
-            if lst[minIdx] > lst[j]:
-                minIdx = j
-        lst[i], lst[minIdx] = lst[minIdx], lst[i]
+def bubbleSrot(arr, arr_len):
+    for i in range(arr_len - 1):
+        for j in range(arr_len -1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 for ts in range(int(input())):
     N = int(input())
     nums = list(map(int, input().split()))
     nums_len = len(nums)
-    selectionSort(nums, nums_len)
+    bubbleSrot(nums, nums_len)
 
     result = []
     for i in range(nums_len-1, -1, -1):
