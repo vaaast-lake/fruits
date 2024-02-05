60934c61e2282e86d2f2eefb9901805150aba060
vaaast-lake <kown9512@gmail.com>
2024-02-06 08:35:28 +0900

    sw_1989__newbie-palindrome.py solve

--- Algorithm_py/sw_1989__newbie-palindrome.py
+++ Algorithm_py/sw_1989__newbie-palindrome.py
@@ -3,12 +3,16 @@ import sys
 sys.stdin = open('./sw_1989__newbie-palindrome__input.txt', 'r')
 
 
-for ts in range(1, int(input)+1):
+for ts in range(1, int(input())+1):
     result = 0
     string = input()
     for i in range(len(string) // 2):
-        if string[i] != string[i + len(string) - 1]:
-        
+        if string[i] != string[len(string) - 1 - i]:
+            break
+    else:
+        result = 1
+
+    print(f'#{ts} {result}')
 
 
 sys.stdin.close()
