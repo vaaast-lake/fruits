707c8239af78f8948354d20840e7da891e6c16f0
민규 <tig04162@gmail.com>
2024-02-05 13:10:56 +0900

    sw_1217__power-of-number solve with tail recursion but not support in python

--- Algorithm_py/sw_1217__power-of-number.py
+++ Algorithm_py/sw_1217__power-of-number.py
@@ -3,12 +3,11 @@ import sys
 sys.stdin = open('./sw_1217__power-of-number__input.txt', 'r')
 
 def r_f(num, power, cumul_num=1):
-    cumul_num *= num
 
-    if power < 2:
+    if power == 0:
         return cumul_num
 
-    return r_f(num, power-1, cumul_num)
+    return r_f(num, power-1, cumul_num * num)
 
 
 for _ in range(1, 11):
