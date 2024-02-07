c9310855bc4658ae183bc0e7ad1829a119e16069
vaaast-lake <kown9512@gmail.com>
2024-02-07 22:22:02 +0900

    sw_2005__Pascal's-Triangle.py solve

--- Algorithm_py/sw_2005__Pascal's-Triangle.py
+++ Algorithm_py/sw_2005__Pascal's-Triangle.py
@@ -4,24 +4,19 @@ sys.stdin = open('./sw_2005__Pascal\'s-Triangle__input.txt', 'r')
 
 for ts in range(1, int(input())+1):
     N = int(input())
-    pasc = [0]*N
-    memo = [0]*N
-    pasc[0] = 1
-    pasc_len = len(pasc)
+    pasc = [[0]*N for _ in range(N)]
+    pasc[0][0] = 1
 
-    i = 0
-    while pasc[pasc_len-1] == 0:
-        if pasc[i+1] == 0:
-            pasc[i+1] = 1
-            i = 0
-            continue
-        else:
-            memo[i] = pasc[i+1]
-            pasc[i+1] = memo[i] + pasc[i]
-            i += 1
+    for i in range(1, N):
+        for j in range(i+1):
+            if j == i:
+                pasc[i][j] = 1
+            pasc[i][j] = pasc[i-1][j-1] + pasc[i-1][j]
 
-    print(pasc)
-
-
+    print(f'#{ts}')
+    for i in range(N):
+        for j in range(i+1):
+            print(pasc[i][j], end=' ')
+        print()
 
 sys.stdin.close()
