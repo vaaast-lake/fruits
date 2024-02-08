44f5cb873dcbceaf09a096b956f080a690ba971e
vaaast-lake <kown9512@gmail.com>
2024-02-08 13:42:18 +0900

    sw_1219__pathfinding.py solve

--- Algorithm_py/sw_1219__pathfinding.py
+++ Algorithm_py/sw_1219__pathfinding.py
@@ -3,22 +3,69 @@ import sys
 sys.stdin = open('./sw_1219__pathfinding__input.txt', 'r')
 
 
-for _ in range(1, 11):
-    path1 = [0]*100
-    path2 = [0]*100
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None]*size
+        self.front = 0
+        self.rear = 0
 
-    ts, path_n = map(int, input().split())
-    lst = list(map(int, input().split()))
-    for i in range(0, path_n, 2):
-        if path1[lst[i]]:
-            path2[lst[i]] = lst[i+1]
+    def enqueue(self, val):
+        if self.is_full():
+            print('queue full')
+            return
+        self.rear = (self.rear+1) % self.size
+        self.queue[self.rear] = val
+
+    def dequeue(self):
+        if self.is_empty():
+            print('queue empty')
+            return
+        self.front = (self.front+1) % self.size
+        return self.queue[self.front]
+
+    def is_empty(self):
+        if self.front == self.rear:
+            return True
         else:
-            path1[lst[i]] = lst[i+1]
+            return False
 
-    stack = []
-    i=0
-    while 1:
-        
+    def is_full(self):
+        if self.front == (self.rear+1) % self.size:
+            return True
+        else:
+            return False
+
+
+def bfs(edges, visit, node):
+    if node == 99:
+        return 1
+
+    visit[node] = True
+    for adj_nd in edges[node]:
+        if not visit[adj_nd]:
+            queue.enqueue(adj_nd)
+
+    if not queue.is_empty():
+        next_node = queue.dequeue()
+        return bfs(edges, visit, next_node)
+    else:
+        return 0
+
+
+for _ in range(1, 11):
+    tc, edge_n = map(int, input().split())
+
+    edges = [[] for _ in range(100)]
+    visit = [False for _ in range(100)]
+    queue = CircularQueue(edge_n)
+
+    data = list(map(int, input().split()))
+    for i in range(0, edge_n*2, 2):
+        edges[data[i]].append(data[i+1])
+
+    result = bfs(edges, visit, 0)
+    print(f'#{tc} {result}')
 
 
 sys.stdin.close()
