a1133174d8ccac5a1c19bcc4c54ba6e4e76c1b09
vaaast-lake <kown9512@gmail.com>
2024-02-10 11:06:10 +0900

    sw_2005__Pascal's-Triangle.py memory optimization

--- Algorithm_py/sw_2005__Pascal's-Triangle.py
+++ Algorithm_py/sw_2005__Pascal's-Triangle.py
@@ -4,19 +4,26 @@ sys.stdin = open('./sw_2005__Pascal\'s-Triangle__input.txt', 'r')
 
 for ts in range(1, int(input())+1):
     N = int(input())
-    pasc = [[0]*N for _ in range(N)]
-    pasc[0][0] = 1
+    pasc = [[0]*n for n in range(1, N+1)]
 
-    for i in range(1, N):
+    for i in range(N):
         for j in range(i+1):
+            if j == 0:
+                pasc[i][j] = 1
+                continue
             if j == i:
                 pasc[i][j] = 1
+                break
             pasc[i][j] = pasc[i-1][j-1] + pasc[i-1][j]
 
     print(f'#{ts}')
-    for i in range(N):
-        for j in range(i+1):
-            print(pasc[i][j], end=' ')
-        print()
+    # for i in range(N):
+    #     for j in range(i+1):
+    #         print(pasc[i][j], end=' ')
+    #     print()
+
+    # print(pasc)
+    for el in pasc:
+        print(*el)
 
 sys.stdin.close()
