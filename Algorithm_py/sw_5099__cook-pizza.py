c0905364bb1399616c47da83e986c9de23514fb6
vaaast-lake <kown9512@gmail.com>
2024-02-16 05:27:59 +0900

    sw_5099__cook-pizza.py solve

--- Algorithm_py/sw_5099__cook-pizza.py
+++ Algorithm_py/sw_5099__cook-pizza.py
@@ -2,68 +2,74 @@ import sys
 
 sys.stdin = open('./sw_5099__cook-pizza__input.txt', 'r')
 
-# class Queue:
-#     def __init__(self, size):
-#         self.size = size
-#         self.front = 0
-#         self.rear = 0
-#         self.s = [0] * self.size
-#
-#     def enqueue(self, v):
-#         if self.is_full():
-#             print('queue is full')
-#             return
-#         self.rear = (self.rear+1) % self.size
-#         self.s[self.rear] = v
-#
-#     def dequeue(self):
-#         if self.is_empty():
-#             print('queue is empty')
-#             return
-#         self.front = (self.front+1) % self.size
-#         return self.s[self.front]
-#
-#     def is_full(self):
-#         return self.front == (self.rear+1) % self.size
-#
-#     def is_empty(self):
-#         return self.front == self.rear
 
+#. queue 구현
+class Queue:
+    def __init__(self, size):
+        self.size = size
+        self.front = 0
+        self.rear = 0
+        self.s = [0] * self.size
 
-def find_last_p(N, M):
-    cnt = 0
-    result = 0
+    def enqueue(self, v):
+        if self.is_full():
+            print('queue is full')
+            return
+        self.rear = (self.rear+1) % self.size
+        self.s[self.rear] = v
 
-    while 1:
-        for i in range(N):
-            if lst[i] == 0:
-                continue
-            lst[i] = lst[i] // 2
-            if lst[i] == 0:
-                if N + 1 < M:
-                    N += 1
-                cnt += 1
-            if cnt == done:
-                result = i + 1
-                return result
-            else:
-                break
+    def dequeue(self):
+        if self.is_empty():
+            print('queue is empty')
+            return
+        self.front = (self.front+1) % self.size
+        return self.s[self.front]
+
+    def is_full(self):
+        return self.front == (self.rear+1) % self.size
+
+    def is_empty(self):
+        return self.front == self.rear
+
 
 for tc in range(1, int(input())+1):
     N, M = list(map(int, input().split()))
     lst = list(map(int, input().split()))
+    #. 큐 요소와 인덱스를 하나의 배열에 묶어줌
+    #. 피자에 테그 붙여주기, 라벨 붙여주기
+    labeling = [[i+1, val] for i, val in enumerate(lst)]
+    q = Queue(N+1)
 
-    done = M-1
+    #. 화덕(queue) 갯수만큼 넣음
+    for i in range(N):
+        q.enqueue(labeling[i])
+
     cnt = 0
-    result = find_last_p(N, M)
+    next_pizz = N
+    #. 하나의 피자가 남을 때까지 반복
+    while cnt < M-1:
+        #. 피자 꺼내서 확인
+        i, cheese = q.dequeue()
+        cheese //= 2
+        #. 다 녹았다면
+        if cheese == 0:
+            #. 녹은 피자 수 카운트
+            cnt += 1
+            #. 다음 피자가 있다면
+            if next_pizz < M:
+                #. 넣어주고
+                q.enqueue(labeling[next_pizz])
+                #. 다음다음 피자 준비
+                next_pizz += 1
+        else:
+            #. 다 안 녹았으면 다시 넣어줌
+            q.enqueue([i, cheese])
+    #. 마지막 남은 피자 꺼내서
+    last_one = q.dequeue()
+    #. 라벨만 출력
+    result = last_one[0]
 
     print(f'#{tc} {result}')
-
-# 5 10
-# 5 9 3 9 9 2 5 8 7 1
-# 2 4 1 4 4
-# 1 2 0(2) 2 2
-# 0(5) 1 1 1 1
 
 
 sys.stdin.close()
