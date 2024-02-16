90d1b8067915b67dd28e6ff9b5d4b4b767bd5bbc
vaaast-lake <kown9512@gmail.com>
2024-02-16 13:40:17 +0900

    sw_3752__possible-test-score.py not solve

--- Algorithm_py/sw_3752__possible-test-score.py
+++ Algorithm_py/sw_3752__possible-test-score.py
@@ -3,40 +3,41 @@ import sys
 sys.stdin = open('./sw_3752__possible-test-score__input.txt', 'r')
 
 
-def comb(visit, cnt, idx=0, sum=0):
+def comb(i, cnt=0, idx=0, sum=0):
     global poss
-    if cnt == N and not poss & 1 << sum:
-        poss |= 1 << sum
-        return
 
     if idx == N:
         return
 
-    comb(visit, cnt+1, idx+1, sum+lst[idx])
-    comb(visit, cnt, idx+1, sum)
+    if cnt == i:
+        poss |= 1 << sum
+        board[idx] |= poss
+        return
+
+    comb(i, cnt+1, idx+1, sum+lst[idx])
+    if not board[idx+1] & 1 << sum:
+        comb(i, cnt, idx+1, sum)
 
 
 for tc in range(1, int(input())+1):
     N = int(input())
     lst = list(map(int, input().split()))
-    visit = [False]*N
     poss = 0
+    board = [0] * (N+1)
 
     for i in range(N):
-        comb(visit, i)
+        comb(i)
 
-    cnt = 1
+    result_cnt = 1
     bit = poss & -poss
     while bit:
         poss -= bit
         bit = poss & -poss
-        cnt += 1
+        result_cnt += 1
 
-    print(f'#{tc} {cnt}')
-
+    print(f'#{tc} {result_cnt}')
 
 sys.stdin.close()
 
 
 
-# 2 2 3 5
