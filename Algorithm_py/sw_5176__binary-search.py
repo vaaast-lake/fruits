e1eaaec3b8d33ba9fb18a16c8935e14e799642e8
박태우 <myxodn12@naver.com>
2024-02-20 16:55:16 +0900

    sw_5176__binary-search.py more compact version

--- Algorithm_py/sw_5176__binary-search.py
+++ Algorithm_py/sw_5176__binary-search.py
@@ -5,29 +5,30 @@ sys.stdin = open('./sw_5176__binary-search__input.txt', 'r')
 def in_order(root):
     global num
 
-    if root:
-        in_order(tree[root][0])
+    if root <= N:
+        in_order(root*2)
         lst[root] = num
         num += 1
-        in_order(tree[root][1])
+        in_order(root*2+1)
 
 
 for tc in range(1, int(input())+1):
     N = int(input())
     num = 1
-    lst = [0] * (N+1)
-    tree = [0] + [[ None ] * 2 for _ in range(N+1)]
-    for i in range(1, N+1):
-        if i == 1:
-            continue
-        if not tree[i//2][0]:
-            tree[i//2][0] = i
-        else:
-            tree[i//2][1] = i
+    lst = [None] * (N+1)
+    # tree = [0] + [[ None ] * 2 for _ in range(N+1)]
+    # for i in range(1, N+1):
+    #     if i == 1:
+    #         continue
+    #     if not tree[i//2][0]:
+    #         tree[i//2][0] = i
+    #     else:
+    #         tree[i//2][1] = i
 
     in_order(1)
 
     print(f'#{tc} {lst[1]} {lst[N//2]}')
     # print(tree)
+    # print(lst)
 
 sys.stdin.close()
