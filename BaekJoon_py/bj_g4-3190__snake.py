d96c4ecaa8c41d5afa79ec7edac12dc0b3762669
vaaast-lake <kown9512@gmail.com>
2024-02-21 07:05:40 +0900

    bj_g4-3190__snake.py add comments

--- BaekJoon_py/bj_g4-3190__snake.py
+++ BaekJoon_py/bj_g4-3190__snake.py
@@ -1,10 +1,11 @@
 import sys
 # from collections import deque
 
-sys.stdin = open('./bj_g4-3190__snake__input.txt', 'r')
+# sys.stdin = open('./bj_g4-3190__snake__input.txt', 'r')
 
 input = sys.stdin.readline
 
+#. Circular queue implementatino
 class Queue:
     def __init__(self, size):
         self.size = size
@@ -31,6 +32,7 @@ class Queue:
     def is_empty(self):
         return self.front == self.rear
 
+    #. for check last factor in q
     def peek(self):
         return self.q[self.rear]
 
@@ -55,21 +57,23 @@ def init_table(apples_pos):
 
 def dummy_move(direction):
     # cur_x, cur_y = d[0]
-    cur_x, cur_y = q.peek()
+    cur_x, cur_y = q.peek()                                             #. 현재 머리 위치 확인
     move_x, move_y = remote_ctr[direction][0], remote_ctr[direction][1]
     next_x, next_y = cur_x + move_x, cur_y + move_y
 
-    if table[next_x][next_y] == WALL or table[next_x][next_y] == BODY:
+    if table[next_x][next_y] == WALL or table[next_x][next_y] == BODY: #. 다음 위치가 벽 혹은 몸이라면 이동 실패
         return False
     else:
-        table[cur_x][cur_y] = BODY
-        if table[next_x][next_y] != APPLE:
+        # table[cur_x][cur_y] = BODY                                   #. 현재 머리 위치 기록 하거나
+                                                                       #. 아래에서 다음 머리 위치를 표시해주기.
+        if table[next_x][next_y] != APPLE:                             #. 사과가 아니라면 마지막 위치를 꼬리로 빼고
+                                                                       #. 해당 위치를 빈 자리로 초기화
             # tail_x, tail_y = d.pop()
             tail_x, tail_y = q.dequeue()
             table[tail_x][tail_y] = EMPTY
         # d.appendleft((next_x, next_y))
-        q.enqueue((next_x, next_y))
-        table[next_x][next_y] = BODY
+        q.enqueue((next_x, next_y))                                  #. 다음 위치를 머리로 넣고 지도에 표시.
+        table[next_x][next_y] = BODY                                 #. 다음 머리 위치 표시.
 
     return True
 
@@ -77,17 +81,17 @@ def dummy_move(direction):
 def start_dummy_game():
     direction = 0
     time = 0
-    for t, vec in vector_info:
-        while time < t:
-            if not dummy_move(direction):
+    for t, vec in vector_info:              #. 리턴으로 중첩 반복문 탈출
+        while time < t:                     #. 첫 번째 시간에 도달할 때까지 이동
+            if not dummy_move(direction):   #. 이동 실패면 시간 하나 더하고 리턴. 아니면 시간 더한 후 반복
                 time += 1
                 return time
             time += 1
-        if vec == RIGHT:
+        if vec == RIGHT:                    #. 시간에 도달했다면 방향 전환
             direction = (direction + 1) % 4
         elif vec == LEFT:
             direction = (direction - 1) % 4
-    else:
+    else:                                   #. 방향 전환 이후에도 이동할 수 있다면 끝날 때까지 이동.
         while 1:
             if not dummy_move(direction):
                 time += 1
