4d05275a9f5eb2b02e1d4091b0d43737ef02f2ad
vaaast-lake <kown9512@gmail.com>
2024-02-22 20:32:27 +0900

    bj_s1-1991__tree-traversal.py solve but has memory problem

--- BaekJoon_py/bj_s1-1991__tree-traversal.py
+++ BaekJoon_py/bj_s1-1991__tree-traversal.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open('./bj_s1-1991__tree-traversal__input.txt', 'r')
+# sys.stdin = open('./bj_s1-1991__tree-traversal__input.txt', 'r')
 
 input = sys.stdin.readline
 print = sys.stdout.write
@@ -10,40 +10,55 @@ LEFT = 1
 RIGHT = 2
 
 
-def in_order(root=1, idx=1):
-    pass
+def in_order(root=1):
+    if root <= 2 ** N - 1:
+        if tree[root * 2]:
+            in_order(root * 2)
+        print(f'{tree[root]}')
+        if tree[root * 2 + 1]:
+            in_order(root * 2 + 1)
 
 
 def pre_order(root=1, idx=1):
-    if root <= N:
-        tree[root] = data[idx][NODE]
+    if root <= 2 ** N - 1:
+        tree[root] = sub_trees[idx][NODE]
         print(f'{tree[root]}')
-        if data[idx][LEFT] != '.':
-            left_idx = dic[data[idx][LEFT]]
+        if sub_trees[idx][LEFT] != '.':
+            left_idx = dic[sub_trees[idx][LEFT]]
             pre_order(root * 2, left_idx)
-        if root * 2 + 1 <= N and data[idx][RIGHT] != '.':
-            right_idx = dic[data[idx][RIGHT]]
+        if root * 2 + 1 <= 2 ** N - 1 and sub_trees[idx][RIGHT] != '.':
+            right_idx = dic[sub_trees[idx][RIGHT]]
             pre_order(root * 2 + 1, right_idx)
 
 
 def post_order(root=1):
+    if root <= 2 ** N - 1:
+        if tree[root * 2]:
+            post_order(root * 2)
+        if tree[root * 2 + 1]:
+            post_order(root * 2 + 1)
+        print(f'{tree[root]}')
     pass
 
 
 N = int(input())
-data = [None] + [input().split() for _ in range(N)]
+sub_trees = [None] + [input().split() for _ in range(N)]
 tree = [None] * (2 ** N)
 
 dic = {}
 
 for i in range(1, N+1):
-    dic.setdefault(data[i][NODE], i)
+    dic.setdefault(sub_trees[i][NODE], i)
 
 pre_order()
+print(f'\n')
+in_order()
+print(f'\n')
+post_order()
 
-print(f'data: {data}\n')
-print(f'dic: {dic}\n')
-print(f'tree: {tree}')
+# print(f'sub_trees: {sub_trees}\n')
+# print(f'dic: {dic}\n')
+# print(f'tree: {tree}')
 
 
 sys.stdin.close()
