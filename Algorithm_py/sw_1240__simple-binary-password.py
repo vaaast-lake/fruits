c86579f16c13df0e98df7da5f0b22e21f1ab16a8
vaaast-lake <kown9512@gmail.com>
2024-02-22 13:30:22 +0900

    today's fruits init

---
+++ Algorithm_py/sw_1240__simple-binary-password.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.stdin = open('./sw_1240__simple-binary-password__input.txt', 'r')
+
+sys.stdin.close()
---
+++ Algorithm_py/sw_5186__binary2.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.stdin = open('./sw_5186__binary2__input.txt', 'r')
+
+sys.stdin.close()
