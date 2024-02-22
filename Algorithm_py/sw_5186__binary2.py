272b39ea8ad9429b53c2996a1a20ea684b32934b
vaaast-lake <kown9512@gmail.com>
2024-02-22 14:01:00 +0900

    sw_5186__binary2.py solve

--- Algorithm_py/sw_5186__binary2.py
+++ Algorithm_py/sw_5186__binary2.py
@@ -2,4 +2,25 @@ import sys
 
 sys.stdin = open('./sw_5186__binary2__input.txt', 'r')
 
+
+for tc in range(1, int(input())+1):
+
+    N = float(input())
+
+    result = ''
+    while N:
+        N = N * 2
+        if N >= 1:
+            N -= 1
+            result += '1'
+        else:
+            result += '0'
+
+        if len(result) > 12:
+            result = 'overflow'
+            break
+
+    print(f'#{tc} {result}')
+
+
 sys.stdin.close()
