256cd81bfcc90dd1f02acdf53b57dd46bebe911e
vaaast-lake <kown9512@gmail.com>
2024-02-23 13:39:58 +0900

    bj_g5-5639__binary-searching-tree solve with div-conq

--- BaekJoon_py/bj_g5-5639__binary-searching-tree.py
+++ BaekJoon_py/bj_g5-5639__binary-searching-tree.py
@@ -1,59 +1,32 @@
 import sys
 
-sys.stdin = open('./bj_g5-5639__binary-searching-tree__input.txt', 'r')
+# sys.stdin = open('./bj_g5-5639__binary-searching-tree__input.txt', 'r')
 
 input = sys.stdin.readlines
 print = sys.stdout.write
-
-NODE = 0
-LEFT = 1
-RIGHT = 2
+sys.setrecursionlimit(20000)
 
 
-def init_subtrees():
-    sub_trees[0][NODE] = data[0]
+def post_order_div_conquer(left, right):
+    if left > right:
+        return
 
-    for i in range(1, data_len):
-        sub_trees[i][NODE] = data[i]
-        if data[i - 1] > data[i]:
-            sub_trees[i - 1][LEFT] = data[i]
-        elif data[i - 1] < data[i]:
-            for j in range(i - 1, 0, -1):
-                if j > 0 and data[j - 1] > data[i] and not sub_trees[j][RIGHT]:
-                    sub_trees[j][RIGHT] = data[i]
-                    break
-            else:
-                if data[0] < data[i] and not sub_trees[0][RIGHT]:
-                    sub_trees[0][RIGHT] = data[i]
-                else:
-                    sub_trees[i-1][RIGHT] = data[i]
+    mid = right+1 #. 더 큰 수가 없을 때 base case로 만들어 주기 위해 right+1로 초기화
+    for i in range(left+1, right+1):
+        if data[left] < data[i]:
+            mid = i
+            break
 
-
-
-def post_order(root=0):
-    if sub_trees[root][LEFT]:
-        post_order(idx_dic[sub_trees[root][LEFT]])
-    if sub_trees[root][RIGHT]:
-        post_order(idx_dic[sub_trees[root][RIGHT]])
-    print(f'{sub_trees[root][NODE]}\n')
+    post_order_div_conquer(left+1, mid-1)
+    post_order_div_conquer(mid, right)
+    print(f'{data[left]}\n')
 
 
 data = list(map(lambda x: int(x.strip()), input()))
 data_len = len(data)
-idx_dic = {}
-for i in range(data_len):
-    idx_dic.setdefault(data[i], i)
 
-sub_trees = [[None] * 3 for _ in range(data_len)]
+# print(f'data: {data}\n')
 
-init_subtrees()
-post_order()
-
-
-print(f'dic: {idx_dic}\n')
-print(f'data: {data}\n')
-print(f'sub_trees: {sub_trees}')
-
-
+post_order_div_conquer(0, data_len-1) #. 실제 인덱스 번호를 기준으로 돌기 위해 인덱스 범위 내의 번호를 넣어줌
 
 sys.stdin.close()
